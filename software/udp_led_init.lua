1e6af2fdf240e6bd3e90c621c5f7aa3401b893c5
ollo <ollo@olloThinkPad>
2015-04-26 15:03:06 +0200

    Version number fixed

--- software/udp_led_init.lua
+++ software/udp_led_init.lua
@@ -1,4 +1,4 @@
-print("Booting... Lightstick v0.10")
+print("Booting... Lightstick v0.30")
 wifi.setmode(wifi.STATION)
 wifi.sta.config("sticknet","stickpw1")
 
@@ -9,6 +9,28 @@ function startUDPServer()
   end)
   s:listen(2342)
   print ("UDP Server started")
+end
+
+function startTelnetServer(dummy)
+	s=net.createServer(net.TCP, 180)
+	s:listen(2323,function(c)
+	global_c=c
+	function s_output(str)
+	  if(global_c~=nil)
+	     then global_c:send(str)
+	  end
+	end
+	node.output(s_output, 0)
+	c:on("receive",function(c,l)
+	  node.input(l)
+	end)
+	c:on("disconnection",function(c)
+	  node.output(nil)
+	  global_c=nil
+	end)
+	print("Welcome to the lightstick. Let the force be with you")
+	end)
+	print("WiFi up and running")
 end
 
 counter=0
@@ -26,21 +48,21 @@ tmr.alarm(1, 200, 1, function()
    else
       print("Connected")
       print( wifi.sta.getip() )
+      -- Display IP on the stick (red = hundreds, green = tenth, blue = ones)
       ws2812.writergb(4, string.char(0, 0, 0):rep(60))
       ip=wifi.sta.getip()
       for k in string.gmatch(ip, "(%d+)") do
 	lastIP=k
       end
-
       hundred=math.floor(lastIP / 100)
       lastIP=lastIP-hundred*100
       tenth=math.floor(lastIP / 10)
       lastIP=lastIP-tenth*10
-      oneth=lastIP
-
-       ws2812.writergb(4, string.char(255, 0, 0):rep(hundred) .. string.char(0,255,0):rep(tenth) .. string.char(0,0,255):rep(oneth) )
+      ones=lastIP
+      ws2812.writergb(4, string.char(255, 0, 0):rep(hundred) .. string.char(0,255,0):rep(tenth) .. string.char(0,0,255):rep(ones) )
 
       startUDPServer()
+      startTelnetServer()
       tmr.stop(1)
    end	
 end)
